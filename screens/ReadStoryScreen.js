dd1d3ad7ab875c00efe0ccbfca678023dd6214b6
YogPanjarale <suryakant.panjarale@gmail.com>
2020-11-22 13:59:38 +0530

    Seach bar added

--- screens/ReadStoryScreen.js
+++ screens/ReadStoryScreen.js
@@ -1,14 +1,98 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,ScrollView } from 'react-native';
+import { SearchBar,Header} from 'react-native-elements';
 
+import db from '../config';
+import firebase from 'firebase';
 
 class ReadStoryScreen extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            storiesList: [],
+            search: '',
+            resultList: []
+        }
+    }
+    componentDidMount = () => {
+        this.getStories()
+    }
+    getStories = async () => {
+        var storiesRef = await db.collection('Stories').get()
+        console.log(storiesRef)
+        storiesRef.forEach(doc => {
+            this.setState({
+                storiesList: [...this.state.storiesList, doc.data()]
+            })
+        });
+        this.setState({
+            resultList: this.state.storiesList
+        })
+    }
+    updateSearch = async () => {
+        if(this.state.search==""||this.state.search==null){
+            this.setState({
+                resultList: this.state.storiesList
+            })
+            return
+        }
+        console.log('updating ....')
+        this.setState({
+            resultList: this.state.storiesList.filter(item => {
+                
+                //console.log(item,item.StoryTitle)
+                if(item.StoryTitle)
+                {var result = item.StoryTitle.toLowerCase().includes(this.state.search.toLowerCase())
+                // console.log(result)
+                return result}
+                return false
+            })
+        })
+
+    }
+    
     render() {
         return (
             <View style={styles.container}>
-                <Text>
-                    ReadStoryScreen
-                </Text>
+                 <Header
+                    backgroundColor={'#39B39C'}
+                    centerComponent={{
+                        text: 'Read Stories',
+                        style: { color: '#fff', fontSize: 20 },
+                    }}
+                />
+                <SearchBar
+                    placeholder="Type Here..."
+                    onChangeText={(val) => {
+                        this.setState({ search: val })
+                        // console.log("  ...."+val+"..... ")
+                        this.updateSearch()
+                    }}
+                    lightTheme
+                    onClear={()=>{this.setState({resultList:this.state.storiesList});
+                    console.log('input cleared')
+                                }}
+                    value={this.state.search}
+                />
+                <Text>{this.state.search}</Text>
+                <ScrollView>
+                {
+                    this.state.resultList.map((doc, index) => {
+                        // console.log(doc)
+                        return (
+                            <TouchableOpacity key={index} style={styles.story}>
+                                <Text>
+                                    StoryTitle: {doc['StoryTitle']}
+                                </Text>
+                                <Text>
+                                    Author: {doc['Author']}
+                                </Text>
+                            </TouchableOpacity>
+
+                        )
+                    })
+                }
+                </ScrollView>
             </View>
         );
     }
@@ -17,9 +101,17 @@ class ReadStoryScreen extends React.Component {
 export default ReadStoryScreen;
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        // flex: 1,
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        //   justifyContent: 'center',
     },
-  });
+    story: {
+        borderBottomWidth: 2,
+        borderColor: '#39B39C',
+        // width: 250,
+        marginVertical: 10,
+        
+
+    }
+});
--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -1,6 +1,17 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    KeyboardAvoidingView,
+    TextInput,
+    TouchableOpacity,
+    ToastAndroid,
+    Alert
+} from 'react-native';
 import { Header } from 'react-native-elements';
+import db from '../config';
+import firebase from 'firebase';
 
 class WriteStoryScreen extends React.Component {
     constructor() {
@@ -9,6 +20,39 @@ class WriteStoryScreen extends React.Component {
             StoryTitle: "",
             Author: "",
             story: "",
+        }
+    }
+    checkStoryExists = async () => {
+        // Create a reference to the cities collection
+        const storiesRef = db.collection('Stories');
+
+        // Create a query against the collection
+        const instances = await storiesRef.where('StoryTitle', '==', this.state.StoryTitle).get();
+        console.log(instances)
+        var isCopy=false
+        instances.forEach((doc) => {
+            console.log(doc.id, ' => ', doc.data());
+            isCopy=doc.data()
+          });
+          isCopy=isCopy==null
+
+        if (isCopy==false) {
+            alert("Story Title is already taken")
+        }
+        console.log(isCopy)
+        return isCopy;
+
+    }
+    submitStory = async () => {
+        if (this.checkStoryExists()) {
+            const res = await db.collection('Stories').add({
+                StoryTitle: this.state.StoryTitle,
+                Author: this.state.Author,
+                story: this.state.story,
+            });
+
+            console.log('Added document with ID: ', res.id);
+            alert("Story Subbmitted Sucessfully ")
         }
     }
     render() {
@@ -21,7 +65,7 @@ class WriteStoryScreen extends React.Component {
                         style: { color: '#fff', fontSize: 20 },
                     }}
                 />
-                <View style={styles.container}>
+                <KeyboardAvoidingView style={styles.container}>
                     <TextInput
                         style={styles.inputBox}
                         onChangeText={(text) => {
@@ -30,7 +74,7 @@ class WriteStoryScreen extends React.Component {
                         value={this.state.StoryTitle}
                         placeholder="Story Title"
                     />
-                      <TextInput
+                    <TextInput
                         style={styles.inputBox}
                         onChangeText={(text) => {
                             this.setState({ Author: text });
@@ -38,21 +82,23 @@ class WriteStoryScreen extends React.Component {
                         value={this.state.Author}
                         placeholder="Author"
                     />
-                      <TextInput
-                        style={[styles.inputBox,{height:200}]}
+                    <TextInput
+                        style={[styles.inputBox, { height: 200 }]}
                         onChangeText={(text) => {
                             this.setState({ story: text });
                         }}
                         value={this.state.story}
                         placeholder="Write Your Story here "
                         multiline={true}
-                        />
-                    <TouchableOpacity style={styles.button}>
+                    />
+                    <TouchableOpacity style={styles.button}
+                        onPress={this.submitStory}
+                    >
                         <Text>
                             Submit
                         </Text>
                     </TouchableOpacity>
-                </View>
+                </KeyboardAvoidingView>
             </View>
         );
     }
@@ -70,19 +116,19 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: '90%',
         alignSelf: 'center',
-         height: 40,
+        height: 40,
         // textAlign: 'center',
 
         borderWidth: 3,
         outline: 'none',
         borderColor: "#aaaaaa"
     },
-    button:{
+    button: {
         backgroundColor: '#FFCA4F',
         paddingHorizontal: 20,
         margin: 20,
-        paddingVertical:10,
+        paddingVertical: 10,
         borderRadius: 5,
-        
+
     }
 });
